cb19ece5d6c4a6b6e3effe435c3ffd5f1fa7a496
fumingtake <fumingtake@gmail.com>
2024-04-15 23:06:36 +0900

    プロジェクト ファイルを追加します。

---
+++ WindowsFormsApp1/Class1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    /// <summary>
+    /// 匿名メソッド
+    /// </summary>
+    internal class Class1
+    {
+        internal void Output(string sPara)
+        {
+
+            //デリゲートで受け取るメソッドを呼び出す
+            Output(WriteHello, sPara);
+        }
+
+        //デリゲートの型
+        internal delegate void WriteString(string s);
+
+        //デリゲートを使用するクラス
+        internal void Output(WriteString method, string s)
+        {
+            method(s);
+        }
+
+        //デリゲートで起動したい処理
+        static void WriteHello(string s)
+        {
+            Console.WriteLine("Hello, " + s);
+        }
+    }
+}
---
+++ WindowsFormsApp1/Class2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static WindowsFormsApp1.Class1;
+
+namespace WindowsFormsApp1
+{
+    /// <summary>
+    /// 匿名メソッドを使う
+    /// </summary>
+    internal class Class2
+    {
+        //Class1のデリゲートを使用
+        internal WriteString moWriteStringHander;
+
+        internal Class2()
+        {
+            moWriteStringHander = new WriteString(WriteHowAreYou);
+        }
+
+        private void WriteHowAreYou(string s)
+        {
+            Console.WriteLine("How are you? " + s);
+        }
+
+        internal void Output(Class1 o, string s)
+        {
+            o.Output(moWriteStringHander, s);
+        }
+    }
+}
